1f8a28af3ba49b74a78303d2178ed8c637670a14
JuanJalif <104107242+JuanJalif@users.noreply.github.com>
2022-04-22 21:14:39 -0300

    Initial commit

---
+++ src/ejercicio3.ts
@@ -0,0 +1,8 @@
+let paso1: string = "cortar un pan por la mitad";
+let paso2: string = "colocar los ingredientes a gusto encima de una mitad";
+let paso3: string = "colocar la otra mitad del pan encima";
+
+console.log("como hacer un sanguche");
+console.log(paso1);
+console.log(paso2);
+console.log(paso3);
